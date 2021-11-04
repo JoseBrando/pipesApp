885e59e4821039d6f26692e9935942f140e50d1d
Brando <jose.brando.c.m@gmail.com>
2021-11-04 09:17:13 -0600

    Theme vela blue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modules PrimeNG
+import { ButtonModule } from 'primeng/button';
+
+// Components
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
